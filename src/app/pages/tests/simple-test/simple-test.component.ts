4965fbc594b8cd9d5a46d49f07a314c4f26c5558
jl91 <plastik69zombieboy@gmail.com>
2020-04-22 14:45:13 -0300

    adding basic structure to detected changes

--- src/app/pages/tests/simple-test/simple-test.component.ts
+++ src/app/pages/tests/simple-test/simple-test.component.ts
@@ -1,13 +1,48 @@
-import {Component, OnInit} from '@angular/core';
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-simple-test',
-  templateUrl: 'simple-test.component.html'
+  templateUrl: 'simple-test.component.html',
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SimpleTestComponent implements OnInit {
-  constructor() {
+export class SimpleTestComponent implements OnInit, AfterViewInit, AfterViewChecked {
+
+  public text = '';
+
+  constructor(
+    private changeDetectionRef: ChangeDetectorRef
+  ) {
   }
 
   ngOnInit() {
   }
+
+  ngAfterViewInit(): void {
+    // this.detectChanges();
+  }
+
+  private detectChanges(): void {
+    this.changeDetectionRef.reattach();
+    this.changeDetectionRef.detectChanges();
+    this.changeDetectionRef.detach();
+  }
+
+  public showText(event: MouseEvent) {
+    event.preventDefault();
+    this.text = 'my text';
+    this.detectChanges();
+  }
+
+  public ngAfterViewChecked(): void {
+    console.log('detected');
+  }
+
+
 }
